8c863d46556aa7a5184c4aeba30a7486a1d951a3
Andrew Dinihan <adinihan44@gmail.com>
2021-11-25 14:18:13 +1100

    changed setup screen to typescript

--- src/screens/Setup.tsx
+++ src/screens/Setup.tsx
@@ -1,26 +1,31 @@
 import { useState } from 'react';
 import pokemon from 'pokemon/all';
+import { TPokemon } from 'pokemon/types';
 
-export default function Setup({ startGame }) {
-    const [gens, setGens] = useState(() => {
+export interface ISetupProps {
+    startGame: (list: Array<TPokemon>) => void;
+};
+
+export default function Setup({ startGame }: ISetupProps) {
+    const [gens, setGens] = useState<{ [key: number]: boolean}>(() => {
         return Object.keys(pokemon).reduce((acc, curr, index) => {
-            if (index === 0) acc[curr] = true;
-            else acc[curr] = false;
+            if (index === 0) acc[curr as unknown as number] = true;
+            else acc[curr as unknown as number] = false;
             return acc;
-        }, {});
+        }, ({} as { [key: number]: boolean}));
     });
     
     const onClickStart = () => {
-        if (Object.keys(gens).every(key => gens[key] === false)) {
+        if (Object.keys(gens).every(key => gens[key as unknown as number] === false)) {
             return alert('You need to select at least 1 generation');
         }
 
         startGame(Object.keys(gens).reduce((acc, curr) => {
-            if (!!gens[curr]) {
-                return [...acc, ...pokemon[curr]];
+            if (!!gens[curr as unknown as number]) {
+                return [...acc, ...pokemon[curr as unknown as number]];
             }
             else return acc;
-        }, []));
+        }, ([] as Array<TPokemon>)));
     };
     
     return (
@@ -42,10 +47,10 @@ export default function Setup({ startGame }) {
                         <div
                             className='genCheckbox noSelect'
                             key={key}
-                            onClick={() => setGens({ ...gens, [key]: !gens[key]})}
+                            onClick={() => setGens({ ...gens, [key]: !gens[key as unknown as number]})}
                             style={{
-                                border: `3px solid ${!!gens[key] ? 'black' : 'grey'}`,
-                                color: !!gens[key] ? 'black' : 'grey',
+                                border: `3px solid ${!!gens[key as unknown as number] ? 'black' : 'grey'}`,
+                                color: !!gens[key as unknown as number] ? 'black' : 'grey',
                             }}
                         >
                             GEN {key}
