cef7b97ebb331e0f46488e97bd495607ea5433f8
Andrew Dinihan <adinihan44@gmail.com>
2023-06-23 21:04:54 +1000

    moved dev dependencies into said property in package.json; updated package.json lock; added full test coverage for root APp file

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -1,10 +1,48 @@
+import renderer from "react-test-renderer";
 import App from "../";
+import Setup from "screens/Setup";
+import Game from "screens/Game";
 
 // Mocked so that ReactDOM.render isn't called, which causes issues in jest env
 jest.mock("react-dom");
 
 describe("App", () => {
-  it("App is defined", () => {
-    expect(App).toBeDefined();
+  it("App should render Setup initially", () => {
+    const component = renderer.create(<App />);
+
+    const testInstance = component.root;
+
+    expect(testInstance.findByType(Setup)).toBeDefined();
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("App should render Game after start is clicked", () => {
+    const component = renderer.create(<App />);
+
+    const testInstance = component.root;
+
+    renderer.act(() => {
+      testInstance.findByProps({ id: "startBtn" }).props.onClick();
+    });
+
+    expect(testInstance.findByType(Game)).toBeDefined();
+  });
+
+  it("App should render Setup after reset is clicked", () => {
+    const component = renderer.create(<App />);
+
+    const testInstance = component.root;
+
+    renderer.act(() => {
+      testInstance.findByProps({ id: "startBtn" }).props.onClick();
+    });
+
+    renderer.act(() => {
+      testInstance.findByProps({ id: "resetBtn" }).props.onClick();
+    });
+
+    expect(testInstance.findByType(Setup)).toBeDefined();
   });
 });
