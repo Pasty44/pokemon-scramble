3958101d7296f0f35b0d3b91853e066bc5396f14
Andrew Dinihan <adinihan44@gmail.com>
2023-07-19 20:44:22 +1000

    updated typing for react and react-dom; updated the react-dom render call to new v18 one

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { useState, StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
 import "styles/general.css";
 import "styles/game.css";
 import "styles/setup.css";
@@ -25,11 +25,7 @@ export default function App() {
   return <Game {...{ pokemonList, reset }} />;
 }
 
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+const domNode = document.getElementById("root");
+if (!domNode) throw new Error("Failed to find the root element");
+const root = createRoot(domNode);
+root.render(<App />);
